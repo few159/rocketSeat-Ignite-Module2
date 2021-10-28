5fe7e2ece555363fa8eea44b88259dbdc397583d
Felipe Albernaz <felipealbernaz@hotmail.com>
2021-10-28 01:16:52 -0300

    minor changes

--- src/components/TransactionsTable/Index.tsx
+++ src/components/TransactionsTable/Index.tsx
@@ -17,6 +17,7 @@ export function TransactionsTable() {
     useEffect(() => {
         api.get('transactions')
             .then(response => setTransactions(response.data.transactions))
+            .then(a => console.log(transactions))
     }, [])
 
     return (
@@ -35,9 +36,9 @@ export function TransactionsTable() {
                     {transactions.map(transaction => (
                         <tr key={transaction.id}>
                             <td>{transaction.title}</td>
-                            <td className={transaction.type}>transaction.amount</td>
-                            <td>transaction.category</td>
-                            <td>transaction.createdAt</td>
+                            <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transaction.amount)}</td>
+                            <td>{transaction.category}</td>
+                            <td>{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}</td>
                         </tr>
                     ))}
                 </tbody>
